c29f911a49f78c340815578aee3e9114bcc804c0
lunah <lunah@farshads-air.lan>
2025-02-11 16:20:30 -0800

    the project now ready to run successfully

---
+++ Develop/Generat-README.md
@@ -0,0 +1,36 @@
+# ppp
+
+![License](https://img.shields.io/badge/License-Apache-2.0-blue.svg)
+
+## Description
+ppp
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+pp
+
+## Usage
+pp
+
+## License
+
+This project is licensed under the Apache-2.0 License.
+
+## Contributing
+pp
+
+## Tests
+pp
+
+## Questions
+For questions, contact me via:
+- GitHub: [pp](https://github.com/pp)
+- Email: pp
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license:',
+        choices: ['MIT', 'GPL-3.0', 'Apache-2.0', 'BSD-3-Clause', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // Logs success or error messages.
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log('❌ Error writing file:', err) : console.log('✅ README.md generated successfully!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // Prompts the user for project details using inquirer
+    inquirer.prompt(questions).then((answers) => {
+        // Generates markdown content using generateMarkdown.js
+        const markdownContent = generateMarkdown(answers);
+        // Writes README.md using fs.writeFile()
+        writeToFile('Generat-README.md', markdownContent);
+    })
+}
 
 // Function call to initialize app
 init();
